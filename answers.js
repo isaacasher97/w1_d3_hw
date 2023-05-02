c4904c93a495fafe49c4524a190e0df1d7f41f38
isaacasher97 <isaac.asher97@gmail.com>
2023-05-02 03:04:55 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -18,23 +18,45 @@
 // Fizz Buzz: This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
 
 // Write a javascript application that logs all numbers from 1 - 100.
-for(let i = 1; i <= 100; i++){
-    console.log(i)
-    if(i % 3 === 0 && i % 5 === 0){ // // If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
-        console.log("FizzBuzz")
-    } else if(i % 5 === 0){ //// If a number is divisible by 5 log "Buzz" instead of the number.
-        console.log("Buzz")
-    } else if(i % 3 === 0){ // // If a number is divisible by 3 log "Fizz" instead of the number.
-        console.log("Fizz")
-    }else {
-        console.log(i)
-    }
-}
+// for(let i = 1; i <= 100; i++){
+//     console.log(i)
+//     if(i % 3 === 0 && i % 5 === 0){ // // If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+//         console.log("FizzBuzz")
+//     } else if(i % 5 === 0){ //// If a number is divisible by 5 log "Buzz" instead of the number.
+//         console.log("Buzz")
+//     } else if(i % 3 === 0){ // // If a number is divisible by 3 log "Fizz" instead of the number.
+//         console.log("Fizz")
+//     }else {
+//         console.log(i)
+//     }
+// }
 ////////////////////////////////
 
 
 ////////////////////////////////
-// Wild Wild Life
+// Wild Wild Life: Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee.splice(2, 1, 5001)
+// console.log(plantee)
+
+// 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy.splice(3, 1, "Gotham City")
+// console.log(wolfy)
+// 3. Give D'Art a second hometown by adding "Hawkins"
+// dart.push("Hawkins")
+// console.log(dart)
+
+// 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+// wolfy.shift()
+// console.log(wolfy)
+
+// wolfy.unshift("Gameboy")
+// console.log(wolfy)
 ////////////////////////////////
 
 
