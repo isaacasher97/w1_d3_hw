eaf0b17e83f931e5116fa95161f1675acf81dbb3
isaacasher97 <isaac.asher97@gmail.com>
2023-05-02 03:03:24 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -8,14 +8,28 @@
 
 ////////////////////////////////
 // Get Even: Write a for loop that will log only the even numbers in 0 through 200
-for(let i = 0; i <= 200; i = i+2){
-    console.log(`I logged every even number, now we're at ${i}`)
+// for(let i = 0; i <= 200; i = i+2){
+//     console.log(`I logged every even number, now we're at ${i}`)
+// }
+////////////////////////////////
+
+
+////////////////////////////////
+// Fizz Buzz: This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+
+// Write a javascript application that logs all numbers from 1 - 100.
+for(let i = 1; i <= 100; i++){
+    console.log(i)
+    if(i % 3 === 0 && i % 5 === 0){ // // If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+        console.log("FizzBuzz")
+    } else if(i % 5 === 0){ //// If a number is divisible by 5 log "Buzz" instead of the number.
+        console.log("Buzz")
+    } else if(i % 3 === 0){ // // If a number is divisible by 3 log "Fizz" instead of the number.
+        console.log("Fizz")
+    }else {
+        console.log(i)
+    }
 }
-////////////////////////////////
-
-
-////////////////////////////////
-// Fizz Buzz
 ////////////////////////////////
 
 
